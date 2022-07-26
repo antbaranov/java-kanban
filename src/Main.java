217741a07696e135ba7b9b343a0dd64a225de6ed
Anton <antnbrnv@yandex.ru>
2022-07-26 16:46:40 +0300

    feat: создать метод обновления данных

--- src/Main.java
+++ src/Main.java
@@ -4,38 +4,27 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        TaskManager taskManager = new TaskManager();
+        Manager manager = new Manager();
 
         System.out.println("Создание задачи");
-        /*System.out.println("Введите название задачи");
-        String taskName = scanner.nextLine();
-        System.out.println("Введите описание задачи");
-        String taskDescription = scanner.nextLine();*/
-        taskManager.creatTask("1 Наименование задачи 1", "1 Описание задачи 1", "NEW");
-        taskManager.creatTask("2 Наименование задачи 2", "2 Описание задачи 2", "NEW");
-        System.out.println(taskManager.tasks);
-        for (Task value : taskManager.tasks.values()) {
-            System.out.println(value);
-        }
+        manager.creatTask("1 Наименование задачи 1", "1 Описание задачи 1", "NEW");
+        manager.creatTask("2 Наименование задачи 2", "2 Описание задачи 2", "NEW");
+        System.out.println(manager.printTasks());
+
         System.out.println("Создание Подзадачи");
-        /*System.out.println("Введите название задачи");
-        String taskName = scanner.nextLine();
-        System.out.println("Введите описание задачи");
-        String taskDescription = scanner.nextLine();*/
-        taskManager.creatSubTask("1 Наименование Подзадачи 1", "1 Описание Подзадачи 1", "NEW");
-        taskManager.creatSubTask("2 Наименование Подзадачи 2", "2 Описание Подзадачи 2", "NEW");
-        System.out.println(taskManager.subTasks);
-        for (SubTask value : taskManager.subTasks.values()) {
-            System.out.println(value);
-        }
+        manager.creatSubTask("1 Наименование Подзадачи 1", "1 Описание Подзадачи 1", "NEW");
+        manager.creatSubTask("2 Наименование Подзадачи 2", "2 Описание Подзадачи 2", "NEW");
+        System.out.println(manager.printSubTasks());
+
 
         System.out.println("Создание Эпика");
-        taskManager.creatEpic("1 Наименование Эпик 1", "1 Описание Эпик 1", "NEW");
-        taskManager.creatEpic("2 Наименование Эпик 2", "2 Описание Эпик 2", "NEW");
-        System.out.println(taskManager.epics);
-        for (Epic value : taskManager.epics.values()) {
-            System.out.println(value);
-        }
+        manager.creatEpic("1 Наименование Эпик 1", "1 Описание Эпик 1", "NEW");
+        manager.creatEpic("2 Наименование Эпик 2", "2 Описание Эпик 2", "NEW");
+        System.out.println(manager.printEpics());
+        // System.out.println(manager.deleteTasks() + " Список должен быть пустой: " + manager.printTasks());
 
+        System.out.println(manager.getByIdTask(1));
+
+     //   System.out.println(manager.updateTask(1, "Обновление Наименования Задачи", "Обновление Описание Задачи", "IN_PROGRESS"));
     }
 }
---
+++ src/Manager.java
@@ -0,0 +1,98 @@
+import java.util.HashMap;
+
+public class Manager {
+    int id;
+
+    // Метод создания простой Задачи task
+    HashMap<Integer, Task> tasks = new HashMap<>();
+
+
+    public Task creatTask(String name, String description, String status) {
+        id++;
+        Task task = new Task(id, name, description, status);
+        tasks.put(id, task);
+        return task;
+    }
+
+    // Метод создания Подзадачи subTask
+    HashMap<Integer, SubTask> subTasks = new HashMap<>();
+
+    public SubTask creatSubTask(String name, String description, String status) {
+        id++;
+        SubTask subTask = new SubTask(id, name, description, status);
+        subTasks.put(id, subTask);
+        return subTask;
+    }
+
+    // Метод создания Эпика Epic
+    HashMap<Integer, Epic> epics = new HashMap<>();
+
+    public Epic creatEpic(String name, String description, String status) {
+        id++;
+        Epic epic = new Epic(id, name, description, status);
+        epics.put(id, epic);
+        return epic;
+    }
+
+    // Получение списка всех Задач
+    public String printTasks() {
+        return "Список задач: " + tasks;
+    }
+
+    // Получение списка всех ПодЗадач
+    public String printSubTasks() {
+        return "Список Подзадач: " + subTasks;
+    }
+
+    // Получение списка всех Эпиков
+    public String printEpics() {
+        return "Список Эпиков: "  + epics;
+    }
+
+    // Удаление всех Задач
+    public String deleteTasks() {
+        tasks.clear();
+        return "Все задачи удалены!";
+    }
+
+    // Удаление всех ПодЗадач
+    public String deleteSubTasks() {
+        subTasks.clear();
+        return "Все Подзадачи удалены!";
+    }
+
+    // Удаление всех Эпиков
+    public String deleteEpics() {
+        subTasks.clear();
+        epics.clear();
+        return "Все Эпики с подзадачами удалены!";
+    }
+
+    // Получение Задач по идентификатору
+    public String getByIdTask(int id) {
+        return "Задача по выбранному id: " + tasks.get(id);
+    }
+
+    // Получение ПодЗадачи по идентификатору
+    public String getByIdSubTask(int id) {
+        return "Подзадача по выбранному id: " + subTasks.get(id);
+    }
+
+    // Получение Эпика по идентификатору
+    public String getByIdEpic(int id) {
+        return "Эпик по выбранному id: " + epics.get(id);
+    }
+
+ // Обновление Задач (Обновление. Новая версия объекта с верным идентификатором передаётся в виде параметра)
+    public void updateTask(Task task) {
+        tasks.put(task.getId(), task);
+        System.out.println("Обновление задачи. Список обновленных задач: " + tasks);
+    }
+
+// Удаление Задачи по идентификатору
+public String deleteByIdTasks(int id) {
+    tasks.clear();
+    return "Задача по id удалена";
+}
+
+} // скобка закрывает class TaskManager
