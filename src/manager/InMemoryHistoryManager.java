a3fe53e4dfef4d1f2e6ae2c3993ca5af92388e7a
Anton <antnbrnv@yandex.ru>
2022-08-11 11:54:26 +0300

    fix: изменить У класса LinkedList есть метод removeFirst() - он удаляет первый элемент списка

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -7,14 +7,14 @@ import java.util.LinkedList;
 
 public class InMemoryHistoryManager implements HistoryManager {
 
-    private final List<Task> tasksHistory = new LinkedList<>();
+    private final LinkedList<Task> tasksHistory = new LinkedList<>();
     private static final int MAX_LIST_SIZE = 10; // Количество записей в списке истории
 
     @Override
     public void add(Task task) {
         tasksHistory.add(task);
         if (tasksHistory.size() > MAX_LIST_SIZE) {
-            tasksHistory.remove(0);
+            tasksHistory.removeFirst();
         }
     }
 
