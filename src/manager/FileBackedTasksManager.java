b6074743094108715851f1a44d30f36c11aa9cc7
Anton <antnbrnv@yandex.ru>
2022-09-13 10:05:27 +0300

    feat: создать необходимые методы без реализации

--- src/manager/FileBackedTasksManager.java
+++ src/manager/FileBackedTasksManager.java
@@ -3,18 +3,47 @@ package manager;
 import tasks.Epic;
 import tasks.SubTask;
 import tasks.Task;
-import tasks.TasksManager;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
-public class FileBackedTasksManager implements TasksManager extends inMemoryTaskManager {
+public class FileBackedTasksManager extends InMemoryTaskManager implements TaskManager {
     private final File file;
 
     public FileBackedTasksManager(File file) {
         this.file = file;
     }
 
+    // Метод сохраняет текущее состояние менеджера в указанный файл
     public void save() {
+
+    }
+
+    // Метод сохранения задачи в строку
+    public String toString(Task task) {
+
+    }
+
+
+    // Метод создания задачи из строки
+    Task fromString(String value) {
+
+    }
+
+    // Метод сохранения менеджера в историю в файл CSV
+    static String historyToString(HistoryManager manager) {
+
+    }
+
+    // Метод восстановления менеджера из истории из файла CSV
+    static List<Integer> historyFromString(String value) {
+        Files.readString(Path.of(path)); // Способ чтения файла
+    }
+
+    // Метод восстанавливает данные менеджера из файла при запуске программы
+    static FileBackedTasksManager loadFromFile(File file) {
 
     }
 
@@ -40,6 +69,11 @@ public class FileBackedTasksManager implements TasksManager extends inMemoryTask
     public void addSubTask(SubTask subTask) {
         super.addSubTask(subTask);
         save(subTask);
+    }
+
+
+    static void main(String[] args) {
+
     }
 
 
