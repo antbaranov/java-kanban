1651e816f58a6e9acb0d10dac745a205b46dc615
Anton <antnbrnv@yandex.ru>
2022-09-15 17:45:10 +0300

    тестирование

--- src/manager/FileBackedTasksManager.java
+++ src/manager/FileBackedTasksManager.java
@@ -35,12 +35,10 @@ public class FileBackedTasksManager extends InMemoryTaskManager {
                 bufferedWriter.write(subTaskToString(subTask) + "\n");
             }
             bufferedWriter.write("\n"); // Добавить пустую строку
-            bufferedWriter.write(HistoryManager.historyToString(getHistoryManager())); // Добавить идентификаторы задач из истории просмотров  HistoryManager.historyToString(historyManager)
-
+            bufferedWriter.write(HistoryManager.historyToString(getHistoryManager()));
         } catch (IOException e) {
             throw new ManagerSaveException("Произошла ошибка во время записи файла");
         }
-
     }
 
     // Метод сохранения Задачи в строку
@@ -92,7 +90,6 @@ public class FileBackedTasksManager extends InMemoryTaskManager {
         }
     }
 
-
     // Метод восстановления менеджера из истории из файла CSV
     static List<Integer> historyFromString(String value) {
         List<Integer> list = new ArrayList<>();
@@ -107,7 +104,6 @@ public class FileBackedTasksManager extends InMemoryTaskManager {
     }
 
     // Метод восстанавливает данные менеджера из файла при запуске программы
-
     public FileBackedTasksManager loadFromFile(File file) {
         final FileBackedTasksManager fileBackedTasksManager = new FileBackedTasksManager();
         try (BufferedReader br = new BufferedReader(new FileReader(file, StandardCharsets.UTF_8))) {
@@ -136,7 +132,6 @@ public class FileBackedTasksManager extends InMemoryTaskManager {
         } catch (IOException e) {
             throw new ManagerSaveException("Произошла ошибка во время чтения файла!");
         }
-
         return fileBackedTasksManager;
     }
 
@@ -294,54 +289,139 @@ public class FileBackedTasksManager extends InMemoryTaskManager {
 
     public static void main(String[] args) {
 
-        FileBackedTasksManager managerFile = new FileBackedTasksManager();
-        FileBackedTasksManager taskManager = Managers.getDefaultFileManager();
+        FileBackedTasksManager managerFile = Managers.getDefaultFileManager();
 
         System.out.println("\nСоздание простой задачи");
-        Task task1 = new Task("1 Наименование простой задачи 1", "1 Описание простой задачи 1", Status.NEW);
-        int task1Id = taskManager.addTask(task1);
-        System.out.println("id простой задачи номер 1 task1Id: " + task1Id);
+        Task task16 = new Task("16 Наименование простой задачи 1", "1 Описание простой задачи 1", Status.NEW);
+        int task16Id = managerFile.addTask(task16);
+        System.out.println("id простой задачи номер 1 task1Id: " + task16Id);
         Task task2 = new Task("2 Наименование простой задачи 2", "2 Описание простой задачи 2", Status.NEW);
-        int task2Id = taskManager.addTask(task2);
+        int task2Id = managerFile.addTask(task2);
         System.out.println("id простой задачи номер 2 task2Id: " + task2Id);
         Task task3 = new Task("3 Наименование простой задачи 3", "3 Описание простой задачи 3", Status.NEW);
-        int task3Id = taskManager.addTask(task3);
+        int task3Id = managerFile.addTask(task3);
         System.out.println("id простой задачи номер 3 task3Id: " + task3Id);
 
         System.out.println("\nСоздание Эпика 1 с 3-мя Подзадачами");
         Epic epic1 = new Epic("1 Наименование Эпик 1", "1 Описание Эпик 1", Status.NEW);
-        int epic1Id = taskManager.addEpic(epic1);
+        int epic1Id = managerFile.addEpic(epic1);
         System.out.println("id Эпика номер 1 epic1Id: " + epic1Id);
 
         System.out.println("Создание Подзадачи 1 Эпика 1");
         SubTask subTask1 = new SubTask("1 Наименование Подзадачи 1", "1 Описание Подзадачи 1", Status.NEW, epic1Id);
-        int subTask1Id = taskManager.addSubTask(subTask1);
+        int subTask1Id = managerFile.addSubTask(subTask1);
         System.out.println("id Подзадачи номер 1 subTask1Id: " + subTask1Id);
 
         System.out.println("Создание Подзадачи 2 Эпика 1");
         SubTask subTask2 = new SubTask("2 Наименование Подзадачи 2", "2 Описание Подзадачи 2", Status.NEW, epic1Id);
-        int subTask2Id = taskManager.addSubTask(subTask2);
+        int subTask2Id = managerFile.addSubTask(subTask2);
         System.out.println("id Подзадачи номер 2 subTask2Id: " + subTask2Id);
 
         System.out.println("Создание Подзадачи 3 Эпика 1");
         SubTask subTask3 = new SubTask("3 Наименование Подзадачи 3", "3 Описание Подзадачи 3", Status.NEW, epic1Id);
-        int subTask3Id = taskManager.addSubTask(subTask3);
+        int subTask3Id = managerFile.addSubTask(subTask3);
         System.out.println("id Подзадачи номер 3 subTask3Id: " + subTask3Id);
 
-        System.out.println(taskManager.getTasks());
-        taskManager.getTaskById(task2Id);
-        taskManager.getTaskById(task3Id);
-        taskManager.getTaskById(task3Id);
-        taskManager.getTaskById(task1Id);
-        taskManager.getEpicById(epic1Id);
-        taskManager.getSubTaskById(subTask1Id);
-        taskManager.getSubTaskById(subTask2Id);
+        System.out.println("\nСоздание Эпика 2 без Подзадачи");
+        Epic epic2 = new Epic("2 Наименование Эпик 2", "2 Описание Эпик 2", Status.NEW);
+        int epic2Id = managerFile.addEpic(epic2);
+        System.out.println("id Эпика номер 2 epic2Id: " + epic2Id);
 
-        System.out.println("Восстановили таски: " + managerFile.getTasks());
-        System.out.println("Восстановили эпики: " + managerFile.getEpics());
-        System.out.println("Восстановили субтаски: " + managerFile.getSubTask());
-        System.out.println("Восстановили историю запросов: " + managerFile.getHistory());
+        /*
+        System.out.println("Создание Подзадачи 3 для 2 Эпика");
+        SubTask subTask3 = new SubTask("Наименование Подзадачи 3", "Описание Подзадачи 3", Status.NEW, epic2Id);
+        int subTask3Id = taskManager.addSubTask(subTask3);
+        System.out.println("id Подзадачи номер 3 subTask3Id: " + subTask3Id);
+        */
 
+        System.out.println("\nВывод списков Эпиков, Задач, Подзадач");
+        System.out.println(managerFile.getEpics());
+        //     System.out.println(taskManager.getTasks());
+        System.out.println(managerFile.getSubTask());
+
+        System.out.println("\nОбновление простой задачи");
+        Task taskUpdate = new Task("Обновили Наименования Задачи 1", "Обновили Описание Задачи 1", Status.IN_PROGRESS);
+        taskUpdate.setId(16);
+        managerFile.updateTask(taskUpdate);
+        managerFile.updateTask(new Task("Обновили Наименования Задачи 2", "Обновили Описание Задачи 2", Status.DONE));
+
+        System.out.println("\nОбновление подзадачи");
+        managerFile.updateSubTask(new SubTask(
+                "Обновили Наименование Подзадачи 1 Эпик 1", "Обновили Описание Подзадачи 1",
+                Status.IN_PROGRESS, epic1Id));
+        managerFile.updateSubTask(new SubTask(
+                "Обновили Наименование Подзадачи 2 Эпик 1", "Обновили Описание Подзадачи 1",
+                Status.DONE, epic1Id));
+        managerFile.updateSubTask(new SubTask(
+                "Обновили Наименование Подзадачи 1 Эпик 2", "Обновили Описание Подзадачи 1",
+                Status.DONE, epic2Id));
+
+        System.out.println("\nВызов методов");
+        System.out.println(managerFile.getTaskById(task2Id));
+        System.out.println("История просмотров16: " + managerFile.getHistory());
+        System.out.println(managerFile.getTaskById(task2Id));
+        System.out.println("История просмотров2: " + managerFile.getHistory());
+        System.out.println(managerFile.getTaskById(task16Id));
+        System.out.println("История просмотров3: " + managerFile.getHistory());
+        System.out.println(managerFile.getTaskById(task3Id));
+        System.out.println("История просмотров4: " + managerFile.getHistory());
+        managerFile.getTaskById(task3Id);
+        managerFile.getTaskById(task3Id);
+        managerFile.getTaskById(task3Id);
+        managerFile.getTaskById(task2Id);
+        managerFile.getTaskById(task2Id);
+        managerFile.getTaskById(task3Id);
+        managerFile.getTaskById(task3Id);
+        managerFile.getTaskById(task16Id);
+        managerFile.getTaskById(task16Id);
+        System.out.println("\nИстория просмотров несколько: " + managerFile.getHistory());
+
+        System.out.println("\nВызов Эпиков");
+        System.out.println(managerFile.getEpicById(epic1Id));
+        System.out.println("История просмотров: " + managerFile.getHistory());
+        System.out.println(managerFile.getEpicById(epic2Id));
+        System.out.println("История просмотров: " + managerFile.getHistory());
+        managerFile.getEpicById(epic1Id);
+        managerFile.getEpicById(epic1Id);
+        managerFile.getEpicById(epic2Id);
+        managerFile.getEpicById(epic2Id);
+        managerFile.getEpicById(epic2Id);
+        managerFile.getSubTaskById(subTask1Id);
+        managerFile.getSubTaskById(subTask2Id);
+        managerFile.getSubTaskById(subTask2Id);
+        System.out.println("История просмотров: " + managerFile.getHistory());
+
+        System.out.println(managerFile.getSubTaskById(subTask1Id));
+        System.out.println("\nИстория просмотров: " + managerFile.getHistory());
+        System.out.println(managerFile.getSubTaskById(subTask3Id));
+        System.out.println("История просмотров: " + managerFile.getHistory());
+        System.out.println(managerFile.getSubTaskById(subTask2Id));
+        System.out.println("История просмотров: " + managerFile.getHistory());
+        System.out.println(managerFile.getSubTaskById(subTask3Id));
+        System.out.println("История просмотров: " + managerFile.getHistory());
+
+        System.out.println(managerFile.getTaskById(task16Id));
+        System.out.println("\nИстория просмотров: " + managerFile.getHistory());
+        System.out.println(managerFile.getTaskById(task3Id));
+        System.out.println("История просмотров: " + managerFile.getHistory());
+        System.out.println(managerFile.getTaskById(task2Id));
+        System.out.println("История просмотров: " + managerFile.getHistory());
+        System.out.println(managerFile.getTaskById(task16Id));
+        System.out.println("История просмотров: " + managerFile.getHistory());
+
+        managerFile.deleteByIdTask(task16Id);
+        managerFile.deleteByIdTask(task2Id);
+        managerFile.deleteByIdTask(task3Id);
+        System.out.println("\nУдалённая задача 1: " + managerFile.getTaskById(task16Id));
+        System.out.println("История просмотров: " + managerFile.getHistory());
+        System.out.println(managerFile.loadFromFile(new File("src/upload/tasks_file.csv")));
+
+        for (Task task : managerFile.getTasks()) {
+            boolean res = task.toString().equals(managerFile.getTasks().get(task.getId()).toString());
+            if (!res) {
+                System.out.println("Не сходятся объекты из файла");
+            }
+        }
     }
 
 }
