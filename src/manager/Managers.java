49f9c4f3581ebf3623b68779382ea8b34ef6969e
Anton <antnbrnv@yandex.ru>
2022-10-10 17:11:18 +0300

    синхронизация

--- src/manager/Managers.java
+++ src/manager/Managers.java
@@ -1,9 +1,16 @@
 package manager;
 
+import server.KVServer;
+import client.HTTPTaskManager;
+
 public class Managers {
 
     public static TaskManager getInMemoryTaskManager(HistoryManager historyManager) {
         return new InMemoryTaskManager(historyManager);
+    }
+
+    public static HTTPTaskManager getDefault(HistoryManager historyManager) {
+        return new HTTPTaskManager(historyManager, "http://localhost:" + KVServer.PORT);
     }
 
     public static HistoryManager getDefaultHistory() {
--- src/server/HttpTaskServer.java
+++ src/server/HttpTaskServer.java
@@ -1,4 +1,20 @@
 package server;
 
+import com.sun.net.httpserver.HttpServer;
+import manager.TaskManager;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
 public class HttpTaskServer {
+    private static final int PORT = 8080;
+    private static final Charset DEFAULT_CHARSET = StandardCharsets.UTF_8;
+    private final TaskManager taskManager;
+    private final HttpServer httpServer;
+
+    public HttpTaskServer() throws IOException, InterruptedException {
+
+    }
+
 }
